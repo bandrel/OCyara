fb708e0b36977d707e468162314b3e80214cf9e1
bandrel <justin@jbollinger.com>
2017-01-12 17:53:44 -0500

    updated test for example pdf

--- tests/test_example_pdf.py
+++ tests/test_example_pdf.py
@@ -1,12 +1,16 @@
 #!/usr/env python3
-from ocyara.ocyara import OCyara
+from OCyara.ocyara import OCyara
 
 ocy = OCyara('Example.pdf')
 ocy.run('example.yara')
 
 
-def num_rules():
+def num_unique_rule_matches():
     return len(ocy.list_rules())
 
+
 def test_number_of_rules():
-    assert num_rules == 3
+    assert num_unique_rule_matches() == 3
+
+def test_example_pdf_rules():
+    assert ocy.list_rules() == {'card', 'SSN', 'credit_card'}
