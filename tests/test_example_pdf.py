9a51d1abd4b7449299515c288190272c66c4c26f
bandrel <justin@jbollinger.com>
2017-01-16 13:10:53 -0500

    updated example yara rules
    Updated example yara rules to contain more specific credit card matching
    Updated test_example_pdf.py to match updated rule set

--- tests/test_example_pdf.py
+++ tests/test_example_pdf.py
@@ -10,7 +10,23 @@ def num_unique_rule_matches():
 
 
 def test_number_of_rules():
-    assert num_unique_rule_matches() == 3
+    assert num_unique_rule_matches() == 9
 
 def test_example_pdf_rules():
-    assert ocy.list_rules() == {'card', 'SSN', 'credit_card'}
+    assert ocy.list_rules() == {'card',
+                                'SSN',
+                                'credit_card',
+                                'JCB',
+                                'Diners_Club',
+                                'Visa',
+                                'American_Express',
+                                'MasterCard',
+                                'Discover'
+                                }
+
+def test_total_number_of_matches():
+    pass
+
+def test_num_matches():
+    pass
+
