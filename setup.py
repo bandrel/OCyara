443d81a9cdf43522a910301e315a9ee398266ac9
ryman1 <ryman1@gmail.com>
2017-01-18 15:24:31 -0500

    prepared to create first wheel

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='OCyara',
     version='0.0.1',
-    packages=[''],
+    packages=find_packages(),
     url='https://github.com/bandrel/OCyara',
-    license='',
-    author='Bandrel',
+    license='GPL v3',
+    author='Bandrel & ryman1',
     author_email='example@example.com',
-    description='A Yara rule engine that scans images for matches using Optical Character Recognition (OCR)'
+    description='A Yara rule engine that scans images for matches using Optical Character Recognition (OCR)',
+    install_requires=['pillow>=4.0.0', 'tqdm', 'cython>=0.25.2', 'tesserocr>=2.1.3', 'yara-python>=3.5.0']
 )
