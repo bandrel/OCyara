cf06147547c26f172a7560fdaded1a5faedbabbb
ryman1 <ryman1@gmail.com>
2017-01-30 13:46:01 -0500

    Added prerequisite info in description for users who don't discover the package via github.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     license='GPL v3',
     author='Bandrel & ryman1',
     author_email='example@example.com',
-    description='A Yara rule engine that scans images for matches using Optical Character Recognition (OCR)',
-        install_requires=['Pillow>=4.0.0', 'tqdm', 'Cython>=0.25.2', 'tesserocr>=2.1.3', 'yara-python>=3.5.0',
-                          'colorlog']
+    description='A Yara rule engine that scans images for matches using Optical Character Recognition (OCR). '
+                'See the Github page for more information about the Cython, Tesseract, and Leptonica prerequsites.',
+    install_requires=['Pillow>=4.0.0', 'tqdm', 'Cython>=0.25.2', 'tesserocr>=2.1.3', 'yara-python>=3.5.0', 'colorlog']
 )
